1d1da04dc8d8653f54e98447455fc750bae8b5c3
Steve <steve.pointon@holcim.co.uk>
2026-01-06 16:57:47 +0000

    Switch to denomailer library for better Deno 2.x compatibility

--- supabase/functions/send-report-email/index.ts
+++ supabase/functions/send-report-email/index.ts
@@ -1,6 +1,6 @@
 import { serve } from "https://deno.land/std@0.168.0/http/server.ts"
 import { createClient } from 'https://esm.sh/@supabase/supabase-js@2'
-import { SmtpClient } from "https://deno.land/x/smtp@v0.10.0/mod.ts"
+import { SmtpClient } from "https://deno.land/x/denomailer@1.6.0/mod.ts"
 
 const GMAIL_USER = Deno.env.get('GMAIL_USER')!
 const GMAIL_APP_PASSWORD = Deno.env.get('GMAIL_APP_PASSWORD')!
